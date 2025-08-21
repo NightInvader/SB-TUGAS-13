2fea71e8d77397d7c99a06739b5963c338a91bb8
NightInvader <muammarsadam13@gmail.com>
2025-08-21 20:34:38 +0700

    Tugas 13

--- controllers/bioskopController.go
+++ controllers/bioskopController.go
@@ -8,28 +8,28 @@ import (
 )
 
 var db *sql.DB
+var err error
 
 type Bioskop struct {
-	ID     int    `json:"id"`
-	Nama   string `json:"nama"`
-	Lokasi string `json:"lokasi"`
-	Rating string `json:"rating"`
+	ID     int     `json:"id"`
+	Nama   string  `json:"nama"`
+	Lokasi string  `json:"lokasi"`
+	Rating float64 `json:"rating"`
 }
 
 var Tempat = []Bioskop{}
 
 func AddBioskop(ctx *gin.Context) {
 	var newTempat Bioskop
-
 	if err := ctx.ShouldBindJSON(&newTempat); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	sqlStatement := `INSERT INTO bioskop (nama, lokasi, rating) VALUES ($1, $2, $3) RETURNING id`
-	err := db.QueryRow(sqlStatement, newTempat.Nama, newTempat.Lokasi, newTempat.Rating)
+	err := db.QueryRow(sqlStatement, newTempat.Nama, newTempat.Lokasi, newTempat.Rating).Scan(&newTempat.ID)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Err().Error()})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
